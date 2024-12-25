43cd3b135c73a02bb5f7b792d25758a987148eeb
emilyj4482 <emilyj4482@gmail.com>
2024-12-25 10:47:05 +0900

    chore: project 생성

---
+++ PracticeNetflix/App/SceneDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  SceneDelegate.swift
+//  PracticeNetflix
+//
+//  Created by EMILY on 25/12/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        window.backgroundColor = .systemBackground
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
+
---
+++ PracticeNetflix/App/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PracticeNetflix
+//
+//  Created by EMILY on 25/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
