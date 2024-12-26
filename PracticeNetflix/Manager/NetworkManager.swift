979e4a715c98afc2b805bdae2b873802191759c0
emilyj4482 <emilyj4482@gmail.com>
2024-12-26 11:16:56 +0900

    add: MainViewModel과 NetworkManager 추가

---
+++ PracticeNetflix/Manager/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  PracticeNetflix
+//
+//  Created by EMILY on 26/12/2024.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
---
+++ PracticeNetflix/ViewModel/MainViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewModel.swift
+//  PracticeNetflix
+//
+//  Created by EMILY on 26/12/2024.
+//
+
+import Foundation
+
+// 넷플릭스 앱의 핵심 비즈니스 로직 : 서버로부터 영화 정보를 불러오는 것
+class MainViewModel {
+    init() {
+        
+    }
+    
+    func fetchPopularMovies() {
+        
+    }
+    
+    func fetchTopRatedMovies() {
+        
+    }
+    
+    func fetchUpcomingMovies() {
+        
+    }
+}
